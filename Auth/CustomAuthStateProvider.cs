43d20ad511ebc42c2fea66cacc8496c3c056f3fa
cesarmjimenez <98713720+cesarmjimenez@users.noreply.github.com>
2026-03-09 17:42:53 -0600

    feat: add login page and redirection

---
+++ Auth/CustomAuthStateProvider.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text.Json;
+
+namespace PanelUserInventoryManagement.Auth;
+
+public class CustomAuthStateProvider(HttpClient http) : AuthenticationStateProvider
+{
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        var identity = new ClaimsIdentity();
+
+        return new AuthenticationState(new ClaimsPrincipal(identity));
+    }
+
+    public void MarkUserAsAuthenticated(string token)
+    {
+        var identity = new ClaimsIdentity(ParseClaimsFromJwt(token), "jwt");
+        var user = new ClaimsPrincipal(identity);
+
+        http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", token);
+
+        NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));
+    }
+
+    private IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
+    {
+        var payload = jwt.Split('.')[1];
+        var jsonBytes = ParseBase64WithoutPadding(payload);
+        var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
+
+        return keyValuePairs!.Select(kvp => new Claim(kvp.Key, kvp.Value.ToString()!));
+    }
+
+    private byte[] ParseBase64WithoutPadding(string base64)
+    {
+        switch (base64.Length % 4)
+        {
+            case 2:
+                base64 += "==";
+                break;
+
+            case 3:
+                base64 += "=";
+                break;
+        }
+
+        return Convert.FromBase64String(base64);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,17 @@
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PanelUserInventoryManagement;
+using PanelUserInventoryManagement.Auth;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<CustomAuthStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(s =>
+s.GetRequiredService<CustomAuthStateProvider>());
 
 builder.Services.AddScoped(sp => new HttpClient
 {
